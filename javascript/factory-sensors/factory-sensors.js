421047765a6de276cc9e048d060f53c8d6431d65
Christopher Story <christory@pm.me>
2024-07-28 19:13:30 -0500

    solve factory sensors problem set

--- javascript/factory-sensors/factory-sensors.js
+++ javascript/factory-sensors/factory-sensors.js
@@ -16,7 +16,7 @@ export class OverheatingError extends Error {
  * @throws {Error}
  */
 export function checkHumidityLevel(humidityPercentage) {
-  throw new Error('Implement the checkHumidity function');
+  if (humidityPercentage > 70) throw new Error('Humidity to high!');
 }
 
 /**
@@ -26,7 +26,9 @@ export function checkHumidityLevel(humidityPercentage) {
  * @throws {ArgumentError|OverheatingError}
  */
 export function reportOverheating(temperature) {
-  throw new Error('Implement the reportOverheating function');
+  if (temperature === null) throw new ArgumentError();
+  if (temperature > 500) throw new OverheatingError(temperature);
+  return temperature;
 }
 
 /**
@@ -41,5 +43,20 @@ export function reportOverheating(temperature) {
  * @throws {ArgumentError|OverheatingError|Error}
  */
 export function monitorTheMachine(actions) {
-  throw new Error('Implement the monitorTheMachine function');
+  try {
+    actions.check();
+  }
+  catch (error) {
+    if (error instanceof ArgumentError) {
+      actions.alertDeadSensor();
+    } else if (error instanceof OverheatingError) {
+      if (error.temperature > 600) {
+        actions.shutdown();
+      } else {
+        actions.alertOverheating();
+      }
+    } else {
+      throw error;
+    }
+  }
 }
